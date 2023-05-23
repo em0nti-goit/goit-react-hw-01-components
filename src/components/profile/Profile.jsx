1647fc85910b9844608dcc464608be00ac73877c
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-23 18:06:05 +0300

    convert styling from vanila css to css-modules

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,20 +1,25 @@
 import { Stats } from '../stats/Stats';
+import scss from './profile.module.scss';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-      <div className="profile__description description">
+    <div className={scss.profile}>
+      <div className={`${scss.profile__description} ${scss.description}`}>
         <img
           src={avatar}
           alt="User avatar"
-          className="description__avatar"
+          className={scss.description__avatar}
           width="100"
           height="100"
         />
-        <div className="description__info">
-          <p className="description__title">{username}</p>
-          <p className="description__text description__text--tag">{tag}</p>
-          <p className="description__text">{location}</p>
+        <div className={scss.description__info}>
+          <p className={scss.description__title}>{username}</p>
+          <p
+            className={`${scss.description__text} ${scss.description__textTag}`}
+          >
+            {tag}
+          </p>
+          <p className={scss.description__text}>{location}</p>
         </div>
       </div>
       <Stats {...stats} />
--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,17 +1,19 @@
+import scss from './stats.module.scss';
+
 export const Stats = ({ followers, views, likes }) => {
   return (
-    <ul className="profile__stats stats">
-      <li className="stats__item">
-        <span className="stats__label">Followers</span>
-        <span className="stats__quantity">{followers}</span>
+    <ul className={scss.stats}>
+      <li className={scss.stats__item}>
+        <span className={scss.stats__label}>Followers</span>
+        <span className={scss.stats__quantity}>{followers}</span>
       </li>
-      <li className="stats__item">
-        <span className="stats__label">Views</span>
-        <span className="stats__quantity">{views}</span>
+      <li className={scss.stats__item}>
+        <span className={scss.stats__label}>Views</span>
+        <span className={scss.stats__quantity}>{views}</span>
       </li>
-      <li className="stats__item">
-        <span className="stats__label">Likes</span>
-        <span className="stats__quantity">{likes}</span>
+      <li className={scss.stats__item}>
+        <span className={scss.stats__label}>Likes</span>
+        <span className={scss.stats__quantity}>{likes}</span>
       </li>
     </ul>
   );
