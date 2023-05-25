46e649c65582b482a6b8c9b2d1f6c94538e92950
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-25 12:04:58 +0300

    update profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,13 @@ import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 export const App = () => {
   return (
     <>
-      <Profile {...user} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics stats={data} title="Upload stats" />
       <Statistics stats={data} />
       <FriendList friends={friends} />
