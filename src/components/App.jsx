43f176fd6bf2bbf077bdd9825239704e300944e8
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-05-25 11:27:13 +0300

    Enable profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-// import user from 'data/user.json';
+import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
-// import { Profile } from 'components/Profile/Profile';
+import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
@@ -10,7 +10,7 @@ import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 export const App = () => {
   return (
     <>
-      {/* <Profile {...user} /> */}
+      <Profile {...user} />
       <Statistics stats={data} title="Upload stats" />
       <Statistics stats={data} />
       <FriendList friends={friends} />
